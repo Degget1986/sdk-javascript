dc80a8a335b3fc09ac50e6f2c18f21514850e7f1
Meet Dave <meet.dave@outlook.com>
2018-10-16 23:06:41 +0530

    Tests for response handlers

--- test/responseHandler.spec.js
+++ test/responseHandler.spec.js
@@ -4,68 +4,45 @@ describe('Response Handler', () => {
 
     describe('rejectResponse should handle reject response', () => {
         it('it should return status === 400', (done) => {
-            rejectResponse('Failure').status === 400;
-            done();
-        })
+            rejectResponse('Failure').status === 400; done();
+        });
     });
 
     describe('successResponse should handle success response', () => {
         it('it should return status === 200', (done) => {
-            successResponse({
-                data: null
-            }).status === 200;
-            done();
-        })
+            successResponse({ data: null }).status === 200; done();
+        });
     });
 
     describe('responseHandler should handle failure response', () => {
         it('it should return status === 400', (done) => {
             const request = {
                 status: 404,
-                response: JSON.stringify({
-                    reason: "sample reason"
-                })
-            }
+                response: JSON.stringify({ reason: "sample reason" })
+            };
             handleResponse(request)
-                .then(response => {
-                    done(response);
-                })
-                .catch(error => {
-                    expect(error.status).to.equal(404);
-                    done();
-                })
-        })
+                .then(response => { done(response); })
+                .catch(error => { expect(error.status).to.equal(404); done(); })
+        });
     });
 
     describe('responseHandler should handle success response', () => {
         it('it should return status === 200', (done) => {
             const request = {
                 status: 200,
-                response: JSON.stringify({
-                    data: null
-                })
+                response: JSON.stringify({ data: null })
             }
             handleResponse(request)
-                .then(response => {
-                    expect(response.status).to.equal(200);
-                    done();
-                })
-                .catch(error => {
-                    done(error);
-                })
-        })
+                .then(response => { expect(response.status).to.equal(200); done(); })
+                .catch(error => { done(error); })
+        });
     });
 
     describe('/GET bundle by ID', () => {
         it('it should throw Bundle ID is missing error', (done) => {
             lib.getBundleById()
-                .then(response => {
-                    done(response);
-                })
-                .catch(error => {
-                    expect(error.status).to.equal(400);
-                    done();
-                })
+                .then(response => { done(response); })
+                .catch(error => { expect(error.status).to.equal(400); done(); })
         }).timeout(15000);
     });
 
